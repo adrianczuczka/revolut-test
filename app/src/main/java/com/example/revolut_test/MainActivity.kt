06c89f09876cde85505400714954fec14e32f5d1
Adrian Czuczka <adrian.czuczka@deliveroo.co.uk>
2020-05-31 13:17:47 +0100

    Added RevolutTestApplication to be used for injection.

--- app/src/main/java/com/example/revolut_test/MainActivity.kt
+++ app/src/main/java/com/example/revolut_test/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.revolut_test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.revolut_test.injection.RevolutTestApplication
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        (applicationContext as RevolutTestApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
-        
     }
 }
---
+++ app/src/main/java/com/example/revolut_test/injection/RevolutTestApplication.kt
@@ -0,0 +1,14 @@
+package com.example.revolut_test.injection
+
+import android.app.Application
+import com.example.revolut_test.MainActivity
+import dagger.Component
+
+@Component
+interface ApplicationComponent {
+    fun inject(mainActivity: MainActivity)
+}
+
+class RevolutTestApplication : Application() {
+    val appComponent = DaggerApplicationComponent.create()
+}
