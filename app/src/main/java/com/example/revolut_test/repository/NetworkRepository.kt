7093699375a2624b5eaf7871b227cfe97d11d401
Adrian Czuczka <adrian.czuczka@deliveroo.co.uk>
2020-05-31 14:37:47 +0100

    Removed unused liveData in NetworkRepository.

--- app/src/main/java/com/example/revolut_test/repository/NetworkRepository.kt
+++ app/src/main/java/com/example/revolut_test/repository/NetworkRepository.kt
@@ -1,8 +1,5 @@
 package com.example.revolut_test.repository
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.example.revolut_test.model.Currency
 import com.example.revolut_test.model.CurrencyNetworkResponse
 import com.example.revolut_test.network.NetworkService
 import io.reactivex.rxjava3.core.Flowable
@@ -11,8 +8,5 @@ import javax.inject.Inject
 class NetworkRepository @Inject constructor(
     private val networkService: NetworkService
 ) {
-    private val _currenciesLiveData = MutableLiveData<List<Currency>>()
-    val currenciesLiveData: LiveData<List<Currency>> = _currenciesLiveData
-
     fun getCurrencies(base: String): Flowable<CurrencyNetworkResponse> = networkService.getCurrencies(base)
 }
