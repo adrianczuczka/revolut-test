a114824eef2be5fe7f04da41a850d11012fe54f3
Adrian Czuczka <adrian.czuczka@deliveroo.co.uk>
2020-06-02 08:38:44 +0100

    Set up view_currency_card.

---
+++ app/src/main/java/com/example/revolut_test/view/CurrencyCardView.kt
@@ -0,0 +1,13 @@
+package com.example.revolut_test.view
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class CurrencyCardView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attributeSet, defStyleAttr) {
+
+}
